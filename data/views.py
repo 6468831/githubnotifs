5a6b7d16bf26523384ff0515aec22a1b8eaf8486
Дмитрий Ульянов <dm1tr.ivanoff@yandex.ru>
2022-12-01 13:46:15 +0300

    first commit

---
+++ data/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+
+@method_decorator(csrf_exempt, name='post')
+class GitCommitView(View):
+    def post(self, request):
+        print(request.body)
+        return request.body
